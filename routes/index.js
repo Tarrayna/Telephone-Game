28a2e0f4761d9af241cf07b253549a8890e8f296
tarrayna <tarraynamg15@gmail.com>
2020-11-05 12:48:14 -0800

    Added future functionality for an error code to display when the wrong room key is entered. Removed some redundancy in .css files Removed calls in .css that only work on some web browsers Updated button size

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Welcome' , error: "Room Key Is Invalid"});
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
   console.log(req.body)
   if(req.body.action === 'createNewRoom')
   {
     console.log("Create New Room")
   }
-  res.render('index', { title: 'Welcome' });
+  res.render('index', { title: 'Welcome'});
 });
 
 module.exports = router;
